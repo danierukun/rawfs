822aca351d1958d79f4f20fac5077e35892a5278
IanBravo <ian_bravo@hotmail.com>
2014-05-17 01:28:51 -0500

    Files and Shell C

---
+++ Files.h
@@ -0,0 +1,14 @@
+//
+//  Files.h
+//  Shell
+//
+//  Created by Ian Bravo on 5/16/14.
+//  Copyright (c) 2014 Ibrarabay. All rights reserved.
+//
+
+#ifndef Shell_Files_h
+#define Shell_Files_h
+
+void FileSize(char* buffer);
+
+#endif
